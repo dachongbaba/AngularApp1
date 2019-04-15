7b35a6d20b4b3b0bf5abbd3c58edf63404f893f7
zhoufei <zhoufeixp@qq.com>
2019-04-15 11:09:39 +0800

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent as DashBoardAppComponent } from '../dashboard/app.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashBoardAppComponent,
+    data: { title: 'DashBoard App' }
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        enableTracing: true
+      })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
