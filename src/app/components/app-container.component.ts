3200db4005877c209a6aad8dc5d10d198fd696f7
zhoufei <zhoufeixp@qq.com>
2019-04-19 02:29:04 +0800

    fix:app-container

--- src/app/components/app-container.component.ts
+++ src/app/components/app-container.component.ts
@@ -1,7 +1,8 @@
-import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
+import {Component, ContentChild, ViewChild, Input, OnChanges} from '@angular/core';
 import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
-
+import { MatToolbar } from '@angular/material/toolbar';
+type Appbar = MatToolbar;
 type SideNav = MatSidenav;
 type SideNavMode = 'over' | 'push' | 'side';
 type SideNavOpen = boolean;
@@ -12,21 +13,40 @@ type SideNavOpen = boolean;
   styleUrls: ['./app-container.component.scss'],
   host: {class: 'd-flex flex-column vh-100'}
 })
-export class AppContainerComponent implements OnInit {
+export class AppContainerComponent {
 
   /* sidenav ref */
-  @ViewChild('leftContainer') leftContainer: ElementRef;
-  @ViewChild('rightContainer') rightContainer: ElementRef;
+  @ContentChild('appbar') appbar: Appbar;
   @ViewChild('leftSideNav') leftSideNav: SideNav;
   @ViewChild('rightSideNav') rightSideNav: SideNav;
-  showLeftSideNav: boolean = false;
-  showRightSideNav: boolean = false;
+
+  private _showAppbar = true;
+  private _showLeftSideNav = true;
+  private _showRightSideNav = true;
+
+  @Input()
+  set showAppbar(val: string) {
+    this._showAppbar = val === 'false' ? false : true;
+  }
+  get showAppbar(): boolean { return this._showAppbar; }
+
+  @Input()
+  set showLeftSideNav(val: string) {
+    this._showLeftSideNav = val === 'false' ? false : true;
+  }
+  get showLeftSideNav(): string { return this._showLeftSideNav; }
+
+  @Input()
+  set showRightSideNav(val: boolean) {
+    this._showRightSideNav = val === 'false' ? false : true;
+  }
+  get showRightSideNav(): boolean { return this._showRightSideNav; }
 
   constructor(breakpointObserver: BreakpointObserver) {
     breakpointObserver.observe([
       Breakpoints.Large
     ]).subscribe(result => {
-      console.log('Breakpoin', result.matches, result.breakpoints)
+      console.log('Breakpoin', result.matches, result.breakpoints);
       if (result.matches) {
         // this.activateWebLayout();
       } else {
@@ -37,35 +57,31 @@ export class AppContainerComponent implements OnInit {
   }
 
   private activateWebLayout() {
-    this.toggleSidenav(this.leftSideNav, 'side', true);
-    this.toggleSidenav(this.rightSideNav, 'side', false);
+    this.togleSidenNav(this.leftSideNav, 'side', true);
+    this.togleSidenNav(this.rightSideNav, 'side', false);
   }
 
   private activateHandsetLayout() {
-    this.toggleSidenav(this.leftSideNav, 'over', false);
-    this.toggleSidenav(this.rightSideNav, 'over', false);
+    this.togleSidenNav(this.leftSideNav, 'over', false);
+    this.togleSidenNav(this.rightSideNav, 'over', false);
   }
 
-  private toggleSidenav(sideNav: SideNav, mode: SideNavMode, open: SideNavOpen) {
+  private togleSidenNav(sideNav: SideNav, mode: SideNavMode, open: SideNavOpen) {
     sideNav.mode = mode;
     sideNav.opened = open;
   }
 
+  private togleSidenNavEx(sideNav: SideNav) {
+    if (sideNav) {
+      sideNav.toggle();
+    }
+  }
   toggleLeft() {
-    this.leftSideNav.toggle();
+    this.togleSidenNavEx(this.leftSideNav);
   }
 
   toggleRight() {
-    this.rightSideNav.toggle();
+    this.togleSidenNavEx(this.rightSideNav);
   }
 
-  ngOnInit(): void {
-    if (!this.leftContainer.nativeElement.hasChildNodes()) {
-      this.showLeftSideNav = true;
-    }
-    if (!this.rightContainer.nativeElement.hasChildNodes()) {
-      this.showRightSideNav = true;
-    }
-    console.log(this.rightContainer);
-  }
 }
