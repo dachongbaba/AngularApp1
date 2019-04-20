38aac99c48bb38f6506435aa76b4c6cd72c2f5ac
zhoufei <zhoufeixp@qq.com>
2019-04-20 15:22:54 +0800

    修改
    Signed-off-by: zhoufei <zhoufeixp@qq.com>

---
+++ src/app/pages/admin/module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ComponentsModule} from '../../components/components.module';
+import {AdminRoutingModule} from './admin-routing.module';
+import {AdminComponent} from './admin.component';
+import {HomeComponent} from './views/home.component';
+import {AboutComponent} from './views/about.component';
+import {PageComponent} from './views/page.component';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    HomeComponent,
+    AboutComponent,
+    PageComponent,
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    AdminRoutingModule,
+  ]
+})
+export class AdminModule {
+}
